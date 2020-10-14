05814e49aed32cfd2c58fe3512e72d4204504606
E <>
2020-10-14 11:47:03 -0400

    initial commit

---
+++ CPPChemistryTutorial/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPPChemistryTutorial
+//
+//  Created by Ethan Zhou on 10/14/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
